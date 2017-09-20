dec3039baea09581764d2a690c689f2a2e7165ec
kensiokahd1 <32090493+kensiokahd1@users.noreply.github.com>
2017-09-20 08:44:46 +0700

    test upload( t.anh )

--- QLTruonghoc/GUI/frmDangky.cs
+++ QLTruonghoc/GUI/frmDangky.cs
@@ -32,7 +32,7 @@ namespace QLTruonghoc
         {
             dataGridView1.AllowUserToAddRows = false;
             txtTen.Enabled = false;
-            con.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=E:\QLTruonghoc\CSDL.accdb;Persist Security Info=False";
+            con.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=F:\VB2.CNTT\CSharp\BaiTap\CSDL.accdb;Persist Security Info=False";
             con.Open();
             OleDbCommand com = new OleDbCommand();
             com.Connection = con;
